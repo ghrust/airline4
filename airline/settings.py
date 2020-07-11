2b01e6e757f9183f2a8600fe78568c07ebba6d3b
ghrust <garaev.rustam.h@gmail.com>
2020-07-11 08:56:27 +0400

    change settings

--- airline/settings.py
+++ airline/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'airline.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DATABASE_NAME'],
-        'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'HOST': os.environ['DATABASE_HOST'],
-        'PORT': os.environ['DATABASE_PORT'],
+        'NAME': os.getenv['DATABASE_NAME'],
+        'USER': os.getenv['DATABASE_USER'],
+        'PASSWORD': os.getenv['DATABASE_PASSWORD'],
+        'HOST': os.getenv['DATABASE_HOST'],
+        'PORT': os.getenv['DATABASE_PORT'],
     }
 }
 
