1041541578df877c2f3aea2fe742e97c2f4b80f8
ghrust <garaev.rustam.h@gmail.com>
2020-07-22 21:13:42 +0400

    some modifications

--- airline/settings.py
+++ airline/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-django_heroku.settings(locals())
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# django_heroku.settings(locals())
